25ea22d6ac16c8136224a57e027c06c3ba93f3ff
Sourav Bose <hudson.sourav@gmail.com>
2021-06-30 08:00:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,23 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.setState={
+			paraGraph:""
+		}
 	};
+
+	paraChange(){
+		this.setState({
+			paraGraph:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+		})
+	}
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={e=>{this.paraChange}}></button>
+				<p id="para">{this.state.paraGraph}</p>
     		</div>
     	);
     }
